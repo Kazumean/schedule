4f5fa3d43392150922f04e484472c14f59d7a2f3
Kazumean <kazumin.angelav@gmail.com>
2023-04-24 16:42:14 +0900

    UsersテーブルとSchedulesテーブルをリレーションで紐づけ。

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Schedule extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'yyymmdd',
+        'user_id',
+    ];
+
+    // リレーションを追加
+    public function users() {
+        return $this->belongsTo(User::class);
+    }
 }
